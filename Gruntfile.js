4f387edadbad048c7aaa203678b7a83498f2e1af
Mitch <hello@liquidvisual.com>
2015-10-28 13:00:50 +1100

    Updating

--- Gruntfile.js
+++ Gruntfile.js
@@ -496,7 +496,7 @@ module.exports = function (grunt) {
   grunt.registerTask('deploy', [
     //'check',
     //'test',
-    // 'build',
+    'build',
     //'buildcontrol:master'
     'buildcontrol:pages',
     ]);
